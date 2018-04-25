dc06946356bce63dbea9260e334193bd2ba6ec42
ana_lazareva <n_lazareva@mail.ru>
2018-04-25 14:35:43 +0300

    IMPR: project settings

--- lib/lwip/port/arch/cc.h
+++ lib/lwip/port/arch/cc.h
@@ -40,7 +40,7 @@ typedef unsigned   short   u16_t;
 typedef signed     short   s16_t;
 typedef unsigned   long    u32_t;
 typedef signed     long    s32_t;
-typedef u32_t mem_ptr_t;
+//typedef u32_t mem_ptr_t;
 typedef int sys_prot_t;
 
 
